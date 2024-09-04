341a4132520b502e20f6c9a9c67d404c14a9e567
Prannaya <prannayagupta@gmail.com>
2024-09-04 09:12:45 +0800

    fix(search): remove the need for iterator

--- src/search.py
+++ src/search.py
@@ -1,48 +1,44 @@
 # src/search.py
 
 from googlesearch import search, SearchResult
-from typing import Generator
+from typing import Generator, Union
 
 from constants import CODE_BLACKLIST
 
 
 class GoogleSearch:
-    def __init__(self, query: str,
+    def __init__(self, query: Union[str, list[SearchResult]],
                  num_results: int = 20,
                  blacklist: list[str] = CODE_BLACKLIST):
-        self.search_results = search(
-            query, num_results=num_results,
-            advanced=True
-        )
+        self.search_results: list[SearchResult]
+        if isinstance(query, str):
+            self.search_results = list(search(
+                query, num_results=num_results,
+                advanced=True
+            ))
+        else:
+            self.search_results = query.copy()[:num_results]
+        
+        self.cap = num_results
         self.blacklist = blacklist
-            
-        self._search_archive: list[SearchResult] = []
+    
+    def copy(self):
+        # because google is dumb and can't handle temperature = 0
+        return GoogleSearch(self.search_results,
+                            num_results=self.cap,
+                            blacklist=self.blacklist)
+        
     
     def urls(self) -> Generator[str, None, None]:
         results = set()
         
-        for result in self._search_archive:
-            # if all(domain not in result.url for domain in self.blacklist) and result.url not in results:
-            yield result.url
-            results.add(result.url)
-        
         for result in self.search_results:
-            self._search_archive.append(result)
-            # if all(domain not in result.url for domain in self.blacklist) and result.url not in results:
-            yield result.url
-            results.add(result.url)
+            if result.url not in results:
+                yield result.url
+                results.add(result.url)
         
         
     def relevant_urls(self) -> Generator[str, None, None]:
-        results = set()
-        
-        for result in self._search_archive:
-            if all(domain not in result.url for domain in self.blacklist) and result.url not in results:
-                yield result.url
-                results.add(result.url)
-        
-        for result in self.search_results:
-            self._search_archive.append(result)
-            if all(domain not in result.url for domain in self.blacklist) and result.url not in results:
-                yield result.url
-                results.add(result.url)
+        for url in self.urls():
+            if all(domain not in url for domain in self.blacklist):
+                yield url
