9bf26374d7308e8fbd121e815168c5c273d087eb
Prannaya <prannayagupta@gmail.com>
2024-09-04 08:42:08 +0800

    feat(search): implement iterator loopback + `src/constants.py`

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+# src/constants.py
+
+__all__ = [
+    "CODE_BLACKLIST"
+]
+
+
+CODE_BLACKLIST = [
+    "stackoverflow.com",
+    "quora.com",
+    "tutorialspoint.com",
+    "w3schools.com",
+    "programiz.com",
+    "javatpoint.com",
+    "geeksforgeeks.org"
+]
