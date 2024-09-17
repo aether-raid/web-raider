a2bcfea7e8adccb3ded0b9b77d09da743abe2a6d
tewenhao <enhaotew@gmail.com>
2024-09-17 16:37:55 +0800

    implement "dfs" article search via recursive calling of `code_urls` with `search_depth` as a limit

--- src/article.py
+++ src/article.py
@@ -2,43 +2,40 @@
 
 from newspaper import Article
 from typing import Generator
-
 from src.codebase import Codebase
 
 
-# CODE_DOMAINS =  [
-#     "github.com",
-#     # "bitbucket.org",
-#     # "gitlab.com"
-# ]
-
 class CodeArticle(Article):
-    def __init__(self, 
-                 url: str, 
-                #  code_domains: list[str] = CODE_DOMAINS,
-                 download: bool = True,
-                 parse: bool = True):
-        super().__init__(url, keep_article_html=True)
-        
-        # self.code_domains = code_domains
-        
+    def __init__(self, url: str, download: bool = True, parse: bool = True):
+        super().__init__(url, keep_article_html=True)        
         if download:
             self.download()
         if parse:
             self.parse()
 
-    def code_urls(self) -> Generator[Codebase, None, None]:
+    # include search_depth to allow for digging of other articles by "DFS"
+    # to a certain extent
+    def code_urls(self, search_depth=1) -> Generator[Codebase, None, None]:
         results = set()
         for elem, key, url, _ in self.clean_top_node.iterlinks():
             if elem.tag != "a":
                 continue
             assert key == "href"
             
-            # if any(domain in url for domain in self.code_domains):
             if Codebase.is_code(url):
                 codebase = Codebase(url)
                 if codebase.repository_url not in results:
                     yield codebase
                     results.add(codebase.repository_url)
-    
-    
+
+            # can change search_depth limit depending on how deep we want to go
+            elif CodeArticle.is_article(url) and search_depth < 3:
+                article = CodeArticle(url)
+                for codebase in article.code_urls(search_depth=search_depth+1):
+                    if codebase.repository_url not in results:
+                        yield codebase
+                        results.add(codebase.repository_url)
+
+    @staticmethod
+    def is_article(self, url):
+        pass
