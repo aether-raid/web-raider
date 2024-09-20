9bfe1f8c8d9a59d3ba823d6ac84c1318420630b3
tewenhao <enhaotew@gmail.com>
2024-09-20 14:30:50 +0800

    add documentation

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,23 @@
 
 import json
 import pandas as pd
+from typing import Any
 
-def json_to_table(json_string):
+def json_to_table(json_string: str) -> str:
     """
-    Note: Somehow, this function only works when AzureOpenAI gpt-4o is used as the model to output in `call_pro_con`
+    Convert a JSON string to a formatted table string.
+
+    Note: Somehow, this function only works when AzureOpenAI gpt-4o is used as the model to output in `call_pro_con`.
+
+    Parameters
+    ----------
+    json_string : str
+        The JSON string to be converted.
+
+    Returns
+    -------
+    str
+        The formatted table as a string.
     """
     # Parse the JSON string
     data = json.loads(json_string)
@@ -28,5 +41,17 @@ def json_to_table(json_string):
     # Return the formatted table as a string
     return df.to_string(index=False)
 
-def useless_func(*_):
+def useless_func(*_: Any) -> None:
+    """
+    A function that does nothing.
+
+    Parameters
+    ----------
+    _ : Any
+        Any number of arguments.
+
+    Returns
+    -------
+    None
+    """
     return
