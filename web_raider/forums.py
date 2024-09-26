60e9a064ab98cbe1d6745f086bed722fb0c99081
tewenhao <enhaotew@gmail.com>
2024-09-26 14:24:51 +0800

    add on forum processing functionality

--- web_raider/forums.py
+++ web_raider/forums.py
@@ -1,5 +1,6 @@
 # src/forums.py
 
+from stackapi import StackAPI
 from enum import Enum
 from urllib.parse import urlparse
 
@@ -9,7 +10,7 @@ class ForumType(str, Enum):
 class Forum:
     def __new__(cls, url: str) -> 'Forum':
         parsed_url = urlparse(url)
-        domain = parsed_url.netloc.lower
+        domain = parsed_url.netloc.lower()
 
         if 'stackoverflow.com' in domain:
             return super(Forum, StackOverflowForum).__new__(StackOverflowForum)
@@ -18,4 +19,30 @@ class Forum:
         self.url = url
 
 class StackOverflowForum(Forum):
-    pass
+    def __init__(self, url):
+        super().__init__(url)
+        self.type = ForumType.STACKOVERFLOW
+        self.question_id = urlparse(url).path.lower().split('/')[2]
+
+        self.SITE = StackAPI('stackoverflow')
+
+    def get_answer_ids(self):
+        answers = self.SITE.fetch('questions/{ids}/answers', ids=[int(self.question_id)], sort='votes')['items']
+        
+        # sort criteria:
+        # 1. push the accepted answer to the front
+        # 2. sort the rest of the answers by their upvotes (ie. score)
+        answers.sort(key = lambda dictionary: (dictionary['is_accepted'] == True), reverse=True)
+        
+        self.answer_ids = []
+        for answer in answers:
+            self.answer_ids.append(answer['answer_id'])
+
+    def get_answer_body(self, answer_id):
+        # filter here to specify that we want the object to be returned along with the body
+        # https://stackapps.com/a/3761
+        answer = self.SITE.fetch('answers/{ids}', ids=[int(answer_id)], filter='withbody')['items']
+
+        # need to parse the body AHHHHHH
+        return answer[0]['body']
+    
