4e2721e21c6f2daf9d2a423eeceaeb7379b2eba0
limyz <limyz.sg@gmail.com>
2024-12-07 21:20:19 +0800

    Added skip parameter to allow main.py to run. Ignore web-raider-venv folder.

--- main.py
+++ main.py
@@ -1076,6 +1076,8 @@ def score_model_answer(question_title: str, cleaned_body: str, model_answer: str
 if __name__ == "__main__":
     print("Running main function")
     path = "../web-raider/first_200_questions.jsonl"
+
+    skip = 0
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     query_count = 0
     with open(path, "r") as file:
